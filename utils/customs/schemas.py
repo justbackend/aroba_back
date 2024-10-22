3f44aaf1029f598e426ca61118f417c220381c1a
user <ahmadboyabdurahimov589@gmail.com>
2024-10-22 10:55:40 +0500

    new changes

--- utils/customs/schemas.py
+++ utils/customs/schemas.py
@@ -1,10 +1,9 @@
 from drf_spectacular.extensions import OpenApiAuthenticationExtension
-from utils.customs import JWTAuthentication
 
 
 class CustomJWTAuthenticationScheme(OpenApiAuthenticationExtension):
     target_class = 'utils.customs.JWTAuthentication'  # To'liq yo'lni tekshiring
-    name = 'CustomJWT'
+    name = 'JWTAuthentication'
     priority = 1
 
     def get_security_definition(self, auto_schema):
