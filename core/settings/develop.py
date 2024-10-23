09ae0e490d300b8718302b2cb4f19502e46350b0
user <ahmadboyabdurahimov589@gmail.com>
2024-10-23 15:20:39 +0500

    new changes

--- core/settings/develop.py
+++ core/settings/develop.py
@@ -6,9 +6,9 @@ CELERY_TASK_ALWAYS_EAGER = True
 INSTALLED_APPS += ['debug_toolbar',]
 
 MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware']
-# REST_FRAMEWORK['DEFAULT_PERMISSION_CLASSES'] = (
-#     'rest_framework.permissions.AllowAny',
-# )
+REST_FRAMEWORK['DEFAULT_PERMISSION_CLASSES'] = (
+    'rest_framework.permissions.AllowAny',
+)
 
 
 DEBUG_TOOLBAR_CONFIG = {
