2193c598dfbf07e9c35dd799585e5c1a1e1254a2
user <ahmadboyabdurahimov589@gmail.com>
2024-10-23 21:33:49 +0500

    new changes

--- apps/users/auth/views.py
+++ apps/users/auth/views.py
@@ -1,9 +1,13 @@
+from drf_spectacular.utils import extend_schema, extend_schema_view
 from rest_framework import generics, status
 from rest_framework.response import Response
 
 from ..auth import serializers
 
 
+@extend_schema_view(
+    post=extend_schema(tags=['auth'])
+)
 class LoginView(generics.GenericAPIView):
     authentication_classes = ()
     permission_classes = ()
@@ -13,4 +17,3 @@ class LoginView(generics.GenericAPIView):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
-
