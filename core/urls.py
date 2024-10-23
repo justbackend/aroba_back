1cc2eeb7871eca819f01725ffc0359a9d6e37682
user <ahmadboyabdurahimov589@gmail.com>
2024-10-23 16:34:47 +0500

    new changes

--- core/urls.py
+++ core/urls.py
@@ -7,7 +7,7 @@ from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("api/v1/users/", include('apps.users.urls')),
+    path("api/v1/", include('apps.v1')),
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
     path('docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
