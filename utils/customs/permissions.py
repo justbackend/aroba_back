6865a1d71e6ed7832642f4d2fcf3e81e3648859e
user <ahmadboyabdurahimov589@gmail.com>
2024-10-23 15:23:39 +0500

    new changes

--- utils/customs/permissions.py
+++ utils/customs/permissions.py
@@ -7,9 +7,9 @@ class RolePermission(permissions.BasePermission):
     def has_permission(self, request, view):
         user = request.user
 
-        if user and user.is_authenticated and getattr(user, 'api_list', None):
+        if api_list := getattr(user, 'api_list', None) and user.is_authenticated:
             path = request.path[user.api_route_len:]
-            for api_info in user.api_list:
+            for api_info in api_list:
 
                 if int(api_info['dynamic']):
                     pattern = f"^{api_info['name'].replace('$', '[^/]+')}$"
