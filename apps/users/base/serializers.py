dbada675656949fa985209d853b9d138cc5aad95
user <ahmadboyabdurahimov589@gmail.com>
2024-10-23 19:43:21 +0500

    new changes

--- apps/users/base/serializers.py
+++ apps/users/base/serializers.py
@@ -2,13 +2,14 @@ from rest_framework import serializers
 from .. import models
 
 
+class ModuleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Module
+        fields = '__all__'
+
+
 class RoleSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Role
         fields = '__all__'
 
-
-class ModuleSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = models.Module
-        fields = '__all__'
