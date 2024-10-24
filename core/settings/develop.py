1f06cd084bfe6e66c09461ff44c77cc170825faf
user <ahmadboyabdurahimov589@gmail.com>
2024-10-24 10:15:49 +0500

    new changes

--- core/settings/develop.py
+++ core/settings/develop.py
@@ -5,7 +5,7 @@ CELERY_TASK_ALWAYS_EAGER = True
 
 INSTALLED_APPS += ['debug_toolbar', ]
 
-MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware']
+MIDDLEWARE.append('debug_toolbar.middleware.DebugToolbarMiddleware')
 REST_FRAMEWORK.update(
     DEFAULT_PERMISSION_CLASSES=('rest_framework.permissions.AllowAny',),
     DEFAULT_RENDERER_CLASSES=(
