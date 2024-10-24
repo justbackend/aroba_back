08d4a2aad50c53e49172b04a08ab0e7eba7b9c1c
user <ahmadboyabdurahimov589@gmail.com>
2024-10-24 11:10:35 +0500

    new changes

--- core/settings/develop.py
+++ core/settings/develop.py
@@ -3,8 +3,7 @@ from .base import *  # noqa
 DEBUG = True
 CELERY_TASK_ALWAYS_EAGER = True
 
-INSTALLED_APPS += ['debug_toolbar', ]
-
+INSTALLED_APPS.append('debug_toolbar')
 MIDDLEWARE.append('debug_toolbar.middleware.DebugToolbarMiddleware')
 REST_FRAMEWORK.update(
     DEFAULT_PERMISSION_CLASSES=('rest_framework.permissions.AllowAny',),
