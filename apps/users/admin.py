133885b9b6449de77faac151278dcf83d41a1fd9
user <ahmadboyabdurahimov589@gmail.com>
2024-10-24 19:26:46 +0500

    new changes

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -4,7 +4,8 @@ from . import models
 
 @admin.register(models.APIRoute)
 class APIAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name')
+    list_display = ('id', 'route', 'name', 'method', 'dynamic')
+    list_display_links = ('id', 'route', 'name', 'method', 'dynamic')
 
 
 @admin.register(models.Role)
