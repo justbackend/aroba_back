ad1c13a1caadeb71041d280bceda518955d9cdeb
user <ahmadboyabdurahimov589@gmail.com>
2024-10-24 18:45:33 +0500

    new changes

--- apps/common/models.py
+++ apps/common/models.py
@@ -5,6 +5,7 @@ from . import managers
 class BaseModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
     objects = managers.Manager()
 
     class Meta:
