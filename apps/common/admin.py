d094e2dba43c8a7e7f6d667bcf2f4439910c4c01
user <ahmadboyabdurahimov589@gmail.com>
2024-10-24 14:17:31 +0500

    new changes

--- apps/common/admin.py
+++ apps/common/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Region)
+class RegionAdmin(admin.ModelAdmin):
+    class PointInline(admin.StackedInline):
+        model = models.Point
+        extra = 1
+
+    list_display = ('id', 'name')
+    inlines = (PointInline,)
+
+
+@admin.register(models.Point)
+class PointAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'region', 'deleted')
