1258a720f92a692aeef8685767286e8b79efdfe1
user <ahmadboyabdurahimov589@gmail.com>
2024-10-24 17:05:07 +0500

    new changes

--- utils/validators.py
+++ utils/validators.py
@@ -2,7 +2,6 @@ import re
 
 from django.core.exceptions import ValidationError
 from django.utils.deconstruct import deconstructible
-from django.core.validators import RegexValidator
 
 
 @deconstructible
@@ -10,9 +9,9 @@ class PhoneValidator:
     message = 'Phone number must be entered in the format: {}'
     code = 'phone_number'
 
-    def __init__(self, pattern: str = r'^\d{12}$', example: str = '999999999999'):
-        self.pattern = pattern
-        self.example = example
+    def __init__(self, len_digits: int = 12):
+        self.pattern = fr'^\d{{{len_digits}}}$'
+        self.example = '9' * len_digits
 
     def __call__(self, value):
         if not re.match(self.pattern, value):
