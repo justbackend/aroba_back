a13e2b54866087bc8f1fc49bba52dcfdcc8680c9
user <ahmadboyabdurahimov589@gmail.com>
2024-10-27 22:13:36 +0500

    new changes

--- utils/__init__.py
+++ utils/__init__.py
@@ -4,10 +4,19 @@ __all__ = (
     'PhoneValidator',
     'VehicleNumberValidator',
     'permission_required_cls',
+    'now',
 )
+
+from datetime import datetime
+from zoneinfo import ZoneInfo
+from django.conf import settings
 
 from .exceptions import APIException
 from .paginations import PageNumberPagination
 from .validators import PhoneValidator, VehicleNumberValidator
 from .decorators import permission_required_cls
+
+
+def now(timezone: str = settings.TIME_ZONE):
+    return datetime.now(ZoneInfo(timezone))
 
