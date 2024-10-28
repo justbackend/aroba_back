f05c7d6397845458c3f0adaaf640967e6590115f
user <ahmadboyabdurahimov589@gmail.com>
2024-10-28 07:02:10 +0500

    new changes

--- apps/orders/admin.py
+++ apps/orders/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Order)
+class OrderAdmin(admin.ModelAdmin):
+    class OrderPaymentInline(admin.StackedInline):
+        model = models.OrderPayment
+        extra = 1
+
+    list_display = ('id', 'code', 'date', 'status', 'total_amount', 'car_number', 'payment_type', 'client')
+    list_display_links = ('id', 'code',)
+    list_filter = ('date', 'status', 'total_amount', 'car_number', 'payment_type', 'client')
+
+
+@admin.register(models.OrderPayment)
+class OrderPaymentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'amount', 'type', 'comment', 'order')
+    list_display_links = ('id', 'type', 'amount')
+    list_filter = ('type', 'order')
+
