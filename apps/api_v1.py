fc38c08e77726e91028a829f4bdffe24293c8cd4
user <ahmadboyabdurahimov589@gmail.com>
2024-10-30 18:06:24 +0500

    new changes

--- apps/api_v1.py
+++ apps/api_v1.py
@@ -4,31 +4,31 @@ urlpatterns = [
     path(
         'users/',
         include('apps.users.urls.base'),
-        name='users'
+        name='users',
     ),
 
     path(
         'auth/',
         include('apps.users.urls.auth'),
-        name='auth'
+        name='auth',
     ),
 
     path(
         'orders/',
         include('apps.orders.urls'),
-        name='orders'
+        name='orders',
         ),
 
     path(
         'clients/',
         include('apps.clients.urls'),
-        name='clients'
+        name='clients',
         ),
 
     path(
         'common/',
         include('apps.common.urls'),
-        name='common'
+        name='common',
     ),
 
 ]
