c997671aa748d207e95910696209049cb7199a0d
user <ahmadboyabdurahimov589@gmail.com>
2024-10-30 13:57:42 +0500

    new changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ python manage.py collectstatic --noinput
 python manage.py migrate --noinput
 
 # Agar gunicorn bn run qilsez
-# exec gunicorn project_name.wsgi:application --bind 0.0.0.0:8000
+ exec gunicorn core.wsgi:application --bind 0.0.0.0:8000
 
 # Agar gunicorn ishlatmasez
-exec python manage.py runserver 0.0.0.0:8000
+#exec python manage.py runserver 0.0.0.0:8000
