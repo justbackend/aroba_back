d55057c6773c9ee221c4409eda3c49fc8e6e5162
user <ahmadboyabdurahimov589@gmail.com>
2024-10-31 18:01:05 +0500

    new changes

--- apps/api_v1.py
+++ apps/api_v1.py
@@ -3,13 +3,13 @@ from django.urls import path, include
 urlpatterns = [
     path(
         'users/',
-        include('apps.users.urls.users'),
+        include('apps.users.main.urls'),
         name='users',
     ),
 
     path(
         'auth/',
-        include('apps.users.urls.auth'),
+        include('apps.users.auth.urls'),
         name='auth',
     ),
 
