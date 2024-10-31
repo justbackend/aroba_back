0919d8ef0076bfd050b99f11bec8e6f302dcd3d4
user <ahmadboyabdurahimov589@gmail.com>
2024-10-31 20:07:34 +0500

    new changes

--- apps/orders/dispatchers/urls.py
+++ apps/orders/dispatchers/urls.py
@@ -3,10 +3,13 @@ from . import views
 
 
 urlpatterns = [
+    # new orders
     path('new-orders/', views.NewOrdersListView.as_view(), name='new-orders'),
 
+    # book order
     path('book-order/<int:order_id>/', views.BookOrRollbackOrderView.as_view(), name='book-orders'),
 
+    # filling orders
     path('filling-orders/', views.FillingOrdersListView.as_view(), name='filling-orders'),
 ]
 
