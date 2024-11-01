547c8ffd993332921ec1256ccf0838f3d66638d2
user <ahmadboyabdurahimov589@gmail.com>
2024-11-01 16:25:39 +0500

    new changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,5 @@ python manage.py migrate --noinput
 # Crontabni o'rnatish
 python manage.py crontab add
 
-# Cron xizmatini ishga tushirish
-cron -f &
-
 # Gunicorn bilan ishlash
 exec gunicorn core.wsgi:application --bind 0.0.0.0:8001 --workers 3
