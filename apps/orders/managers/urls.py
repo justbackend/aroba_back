63c14d7a0ef525c20b356b73a33d2f8cfcae0b60
user <ahmadboyabdurahimov589@gmail.com>
2024-11-01 19:12:33 +0500

    new changes

--- apps/orders/managers/urls.py
+++ apps/orders/managers/urls.py
@@ -5,7 +5,8 @@ from . import views
 urlpatterns = [
     path('additional-amount/<int:order_id>/', views.AdditionalAmountView.as_view(), name='additional-amount'),
     path('status-orders/', views.StatusOrdersListView.as_view(), name='status-orders'),
-path('status-orders/<int:pk>/', views.UpdateOrderStatusView.as_view(), name='status-orders-update'),
+    path('status-orders/<int:pk>/', views.UpdateOrderStatusView.as_view(), name='status-orders-update'),
+    path('delete-order/<int:pk>/', views.DeleteOrderStatusView.as_view(), name='status-orders-update'),
     path('rollback-order/<int:order_id>/', views.RollbackOrderView.as_view(), name='status-orders'),
 ]
 
