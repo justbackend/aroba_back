dbf9f97dea0ef0083a47c9861d5642733f3b239f
user <ahmadboyabdurahimov589@gmail.com>
2024-11-01 22:14:00 +0500

    new changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,4 +11,4 @@ python manage.py migrate --noinput
 #cron -f &
 
 # Asosiy dasturlarni ishga tushirish
-exec gunicorn core.wsgi:application --bind 0.0.0.0:8001 --workers 4
+exec gunicorn core.wsgi:application --bind 0.0.0.0:8001 --workers 4 --preload
