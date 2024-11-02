8437efcdcc2c2bd0dcf9fcbbee54758d7fd528f3
user <ahmadboyabdurahimov589@gmail.com>
2024-11-02 13:43:40 +0500

    new changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,6 +9,7 @@ python manage.py crontab add
 
 # Cron xizmatini to'g'ridan-to'g'ri sessiyada ishga tushirish
 #cron -f &
+service cron start
 
 # Asosiy dasturlarni ishga tushirish
 exec gunicorn core.wsgi:application --bind 0.0.0.0:8001 --workers 4 --preload
