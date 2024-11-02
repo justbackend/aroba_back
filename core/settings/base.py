cda70319126ddf96d6615fdf75d2cf651ded3f0c
user <ahmadboyabdurahimov589@gmail.com>
2024-11-02 11:17:32 +0500

    new changes

--- core/settings/base.py
+++ core/settings/base.py
@@ -9,10 +9,12 @@ https://docs.djangoproject.com/en/4.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
+import os
 from datetime import timedelta
 from pathlib import Path
 
 import environ
+from dotenv import load_dotenv
 
 from core.jazzmin import *  # noqa
 from ..cronjobs import CRON_TASKS  # noqa
@@ -23,6 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # READING ENV
 env = environ.Env()
 env.read_env(".env")
+
+# READING ENV
+load_dotenv(dotenv_path=BASE_DIR / ".env")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
