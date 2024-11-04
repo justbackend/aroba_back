ada18cb1f8e4e17d5c5dd176d2b913493bd45ea9
user <ahmadboyabdurahimov589@gmail.com>
2024-11-04 15:16:02 +0500

    new changes

--- apps/clients/serializers.py
+++ apps/clients/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from . import models
+from apps.common import models as common_models
 import utils
+from utils import choices
 
 
 class RouteListSerializer(serializers.Serializer):
@@ -11,9 +13,40 @@ class RouteListSerializer(serializers.Serializer):
 
 
 class ClientSerializer(serializers.ModelSerializer):
+    loading = serializers.PrimaryKeyRelatedField(
+        write_only=True, queryset=common_models.Point.active_objects.all()
+    )
+    unloading = serializers.PrimaryKeyRelatedField(
+        write_only=True, queryset=common_models.Point.active_objects.all()
+    )
+    amount = serializers.IntegerField(read_only=True, min_value=1)
+    type = serializers.ChoiceField(choices=choices.ClientRouteTypes.choices)
+
     class Meta:
         model = models.Client
-        fields = '__all__'
+        fields = (
+            'id',
+            'name',
+            'phone',
+            'requisite',
+            'requisite_file',
+            'accounting_phone',
+            'loading',
+            'unloading',
+            'type',
+            'amount'
+        )
+
+
+    def create(self, validated_data):
+        loading = validated_data.pop('loading', )
+        unloading = validated_data.pop('unloading', )
+        amount = validated_data.pop('amount', )
+        _type = validated_data.pop('type', )
+        obj = models.Client(**validated_data)
+
+        models.ClientRoute.objects.create(loading=loading, unloading=unloading, amount=amount, type=_type)
+        return obj
 
 
 class ClientListSerializer(serializers.ModelSerializer):
