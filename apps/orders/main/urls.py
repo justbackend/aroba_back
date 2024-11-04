f98479b06d4bc52dd483cdd7a486de53509bf2cb
user <ahmadboyabdurahimov589@gmail.com>
2024-11-04 15:37:16 +0500

    new changes

--- apps/orders/main/urls.py
+++ apps/orders/main/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+
     # create order
     path('create-order/', views.CreateOrderView.as_view(), name='create-order'),
     path('clients/', views.ClientsListView.as_view(), name='create-order'),
