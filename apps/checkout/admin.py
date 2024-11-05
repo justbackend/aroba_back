3c23efbb09074fc6109b1c7ed9f70f4d5dbf39ff
user <ahmadboyabdurahimov589@gmail.com>
2024-11-05 14:19:25 +0500

    new changes

--- apps/checkout/admin.py
+++ apps/checkout/admin.py
@@ -5,9 +5,17 @@ from . import models
 
 
 @admin.register(models.Checkout)
-class CheckoutAdmin(admin.ModelAdmin):
+class CheckoutAdmin(u_admin.ModelAdmin):
     list_display = ('id', 'name', 'balance', 'created_at', 'updated_at')
 
     def has_add_permission(self, request): return False
 
     def has_delete_permission(self, request, obj=None): return False
+
+
+@admin.register(models.Transaction)
+class TransactionAdmin(u_admin.ModelAdmin):
+    list_display = ('id', 'amount', 'type', 'status', 'created_at', 'updated_at')
+
+
+
