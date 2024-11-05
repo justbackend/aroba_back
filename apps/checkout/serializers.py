0910be377781f5dc11f83e0a064aa513248e9999
user <ahmadboyabdurahimov589@gmail.com>
2024-11-05 13:30:41 +0500

    new changes

--- apps/checkout/serializers.py
+++ apps/checkout/serializers.py
@@ -1,6 +1,24 @@
 from rest_framework import serializers
 
 
+class ReportOrdersSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    loading_name = serializers.CharField(read_only=True)
+    unloading_name = serializers.CharField(read_only=True)
+    car_number = serializers.CharField(read_only=True)
+    income = serializers.IntegerField(read_only=True)
+    total_amount = serializers.IntegerField(read_only=True)
+
+
 class ReportSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     name = serializers.CharField()
+    amounts = serializers.SerializerMethodField()
+    orders = ReportOrdersSerializer(many=True)
+
+    def get_amounts(self, obj):
+        result = dict(sum_income=0, sum_total_amount=0)
+        for order in obj.orders.all():
+            result["sum_income"] += order.income or 0
+            result["sum_total_amount"] += order.total_amount or 0
+        return result
--- apps/checkout/views.py
+++ apps/checkout/views.py
@@ -1,9 +1,13 @@
+from django.db.models import Prefetch, F, Sum, Q, OuterRef, Subquery
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.response import Response
 from rest_framework import views, generics
+
+import utils
 from . import models, serializers
 from apps.orders import models as order_models
 from apps.clients import models as client_models
+from utils.choices import *
 
 
 class ReportOrdersListAPI(generics.ListAPIView):
@@ -12,6 +16,23 @@ class ReportOrdersListAPI(generics.ListAPIView):
     filterset_fields = ('orders__payment_type',)
 
     def get_queryset(self):
+        if not (payment_type := self.request.query_params.get('orders__payment_type')):
+            raise utils.APIException('Must provide payment type')
 
-        qs = client_models.Client.objects.filter(orders__isnull=False).distinct()
-        return qs
+        orders_qs = (
+            order_models.Order.objects.filter(
+                payment_type=payment_type,
+                status__in=(OrderStatus.STARTED, OrderStatus.AT_FACTORY,
+                            OrderStatus.LOADED, OrderStatus.LOCATION_ASSIGNED)
+            )
+            .annotate(loading_name=F('loading__name'), unloading_name=F('unloading__name'))
+        )
+
+        return (
+            client_models.Client.objects
+            .prefetch_related(Prefetch('orders', queryset=orders_qs))
+            .annotate(
+            )
+            .filter(orders__isnull=False)
+            .distinct()
+        )
