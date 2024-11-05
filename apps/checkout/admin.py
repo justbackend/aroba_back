504f554f5e80990b13c30d8b316e011618a6587c
user <ahmadboyabdurahimov589@gmail.com>
2024-11-05 14:17:26 +0500

    new changes

--- apps/checkout/admin.py
+++ apps/checkout/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from unfold import admin as u_admin
 
-# Register your models here.
+from . import models
+
+
+@admin.register(models.Checkout)
+class CheckoutAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'balance', 'created_at', 'updated_at')
+
+    def has_add_permission(self, request): return False
+
+    def has_delete_permission(self, request, obj=None): return False
