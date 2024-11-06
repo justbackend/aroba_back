2e2110f6b03dd0e37ff5ae81207e1b37c7c3f2e2
user <ahmadboyabdurahimov589@gmail.com>
2024-11-06 16:40:42 +0500

    new changes

--- apps/accountant/urls.py
+++ apps/accountant/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+
+    # accountant orders
     path('orders/', views.AccountantOrderList.as_view(), name='accountant-orders'),
     path('orders-excel/', views.AccountantOrdersExcel.as_view(), name='accountant-orders'),
+
+    # report orders
     path('finished-orders/', views.FinishedOrders.as_view(), name='accountant-finished-orders')
 ]
