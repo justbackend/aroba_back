3b41cadccb8c6ae922b343082f6b062c41be8ba0
user <ahmadboyabdurahimov589@gmail.com>
2024-11-06 10:38:29 +0500

    new changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 set -e
 
-# Django migrations
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
-# Crontab qo'shish
 python manage.py crontab add
 
-# Cron xizmatini to'g'ridan-to'g'ri sessiyada ishga tushirish
-#cron -f &
 service cron start
 
-# Asosiy dasturlarni ishga tushirish
 exec gunicorn core.wsgi:application --bind 0.0.0.0:8001 --workers 4 --preload
 
 
