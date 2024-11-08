47df6b86bf5004ce93f364fee28778666f5254d7
user <ahmadboyabdurahimov589@gmail.com>
2024-11-08 17:54:44 +0500

    new changes

--- apps/accountant/urls.py
+++ apps/accountant/urls.py
@@ -4,8 +4,7 @@ from . import views
 urlpatterns = [
 
     # accountant orders
-    path('orders/', views.AccountantOrderList.as_view(), name='accountant-orders'),
-    path('orders-excel/', views.AccountantOrdersExcel.as_view(), name='accountant-orders'),
+    path('clients/', views.TransClientsViewList.as_view(), name='accountant-orders'),
 
     # report orders
     path('finished-orders/', views.FinishedOrders.as_view(), name='accountant-finished-orders'),
--- apps/accountant/views.py
+++ apps/accountant/views.py
@@ -2,46 +2,21 @@ from django.db.models import F
 from rest_framework import generics
 
 from apps.orders import models as orders_models
+from apps.clients import models as client_models
 from utils.choices import *
 from . import serializers
 from utils.excel import ExcelListView
 
 
-class AccountantOrderList(generics.ListAPIView):
-    serializer_class = serializers.AccountantOrdersSerializer
+class TransClientsViewList(generics.ListAPIView):
+    serializer_class = serializers.TransClientSerializer
 
     def get_queryset(self):
         return (
-            orders_models.Order.objects
-            .select_related('loading', 'unloading', 'client')
-            .filter(
-                payment_type=OrderPaymentTypes.TRANSFER,
-                status__in=(OrderStatus.STARTED, OrderStatus.LOADED,
-                            OrderStatus.AT_FACTORY, OrderStatus.LOCATION_ASSIGNED)
-            )
+            client_models.Client.objects
+            .filter(routes__type=ClientRouteTypes.TRANSFER)
+            .distinct()
         )
-
-
-class AccountantOrdersExcel(ExcelListView, AccountantOrderList):
-    filename = 'accountant_orders'
-    fields = (
-        'id', 'code', 'client_name', 'client_phone',
-        'client_accounting_phone', 'loading_name', 'unloading_name', 'total_amount'
-    )
-    default_widths = 30
-
-    def get_data(self):
-        return (
-            self.get_queryset()
-            .annotate(
-                client_name=F('client__name'),
-                client_phone=F('client__phone'),
-                client_accounting_phone=F('client__accounting_phone'),
-                loading_name=F('loading__name'),
-                unloading_name=F('unloading__name'),
-            )
-        )
-
 
 class FinishedOrders(generics.ListAPIView):
     serializer_class = serializers.FinishedOrdersSerializer
@@ -64,6 +39,7 @@ class FinishedOrdersExcel(ExcelListView, FinishedOrders):
         'unloading_name', 'car_number', 'total_amount'
     )
     default_widths = 30
+
     def get_data(self):
         return (
             self.get_queryset()
