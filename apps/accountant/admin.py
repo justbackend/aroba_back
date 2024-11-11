5dfa95e6be2a7c397a396908307b37b4b748996e
user <ahmadboyabdurahimov589@gmail.com>
2024-11-11 14:43:50 +0500

    new changes

--- apps/accountant/admin.py
+++ apps/accountant/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
+from unfold import admin as u_admin
 from . import models
 
 
 @admin.register(models.AccountantInvoice)
-class AccountantInvoiceAdmin(admin.ModelAdmin):
+class AccountantInvoiceAdmin(u_admin.ModelAdmin):
     list_display = ("id", "customer", "accounting_phone", 'inn', 'created_at')
