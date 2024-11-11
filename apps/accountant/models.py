a8779878f1360677026678ed79ac72a38a88be78
user <ahmadboyabdurahimov589@gmail.com>
2024-11-11 14:38:29 +0500

    new changes

--- apps/accountant/models.py
+++ apps/accountant/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from utils import BaseModel, PhoneValidator
 
-# Create your models here.
+
+class AccountantInvoice(BaseModel):
+    customer = models.CharField('Customer', max_length=50)
+    inn = models.CharField(max_length=15, verbose_name="Inner client")
+    accounting_phone = models.CharField(
+        max_length=20, verbose_name="Accounting phone number",
+        validators=[PhoneValidator()]
+    )
+
+    client = models.ForeignKey(
+        'clients.Client', on_delete=models.PROTECT, related_name='invoices', verbose_name='Client'
+    )
+
+    class Meta:
+        verbose_name = 'Invoice'
+        verbose_name_plural = 'Invoices'
+        db_table = 'accountant_invoices'
