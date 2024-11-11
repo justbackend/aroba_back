cc3567f13006496386d8884a6ee7fa1e0e5a0698
user <ahmadboyabdurahimov589@gmail.com>
2024-11-11 14:45:17 +0500

    new changes

--- apps/accountant/admin.py
+++ apps/accountant/admin.py
@@ -1,8 +1,15 @@
 from django.contrib import admin
 from unfold import admin as u_admin
 from . import models
+from apps.orders.models import Order
 
 
 @admin.register(models.AccountantInvoice)
 class AccountantInvoiceAdmin(u_admin.ModelAdmin):
+
+    class OrdersInline(u_admin.StackedInline):
+        model = Order
+        extra = 0
+
     list_display = ("id", "customer", "accounting_phone", 'inn', 'created_at')
+    inlines = (OrdersInline,)
