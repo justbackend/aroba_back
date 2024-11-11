048549548714287efb32f37df2624b0bccd0f6c0
user <ahmadboyabdurahimov589@gmail.com>
2024-11-11 14:42:54 +0500

    new changes

--- apps/accountant/admin.py
+++ apps/accountant/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.AccountantInvoice)
+class AccountantInvoiceAdmin(admin.ModelAdmin):
+    list_display = ("id", "customer", "accounting_phone", 'inn', 'created_at')
