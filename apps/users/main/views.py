8f8fad71729d83fce51bc1fd56f6cc3e79813e8f
user <ahmadboyabdurahimov589@gmail.com>
2024-11-12 16:28:12 +0500

    new changes

--- apps/users/main/views.py
+++ apps/users/main/views.py
@@ -1,7 +1,6 @@
 from rest_framework import viewsets
 from rest_framework.parsers import MultiPartParser, FormParser
 
-from utils.permissions import RolePermission
 from . import serializers
 from .. import models
 
