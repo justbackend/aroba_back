cca190e90b8d5d16a3a25415f7a374840762a4ad
user <ahmadboyabdurahimov589@gmail.com>
2024-11-13 20:18:45 +0500

    new changes

--- apps/orders/consumers.py
+++ apps/orders/consumers.py
@@ -1,12 +1,9 @@
 import json
 from channels.generic.websocket import AsyncWebsocketConsumer
-from apps.OrderApp.functions import async_get_user
 
 
 class DispatcherOrdersConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        self.scope['user'] = await async_get_user(self)
-
         await self.accept()
         await self.channel_layer.group_add('dispatcher_group', self.channel_name)
 
@@ -35,9 +32,7 @@ class FillingOrdersConsumer(AsyncWebsocketConsumer):
 
 class StatusOrdersConsumer(AsyncWebsocketConsumer):
     async def connect(self):
-        user = await async_get_user(self)
-        self.scope['user'] = user
-        await self.channel_layer.group_add(f"status_group_{user.id}", self.channel_name)
+        await self.channel_layer.group_add(f"status_group", self.channel_name)
         await self.accept()
 
     async def disconnect(self, close_code):
