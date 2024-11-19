5d649655abadf5c6a00fbec13417f37200e5cd0c
user <ahmadboyabdurahimov589@gmail.com>
2024-11-19 14:04:52 +0500

    new changes

--- apps/checkout/imb/views.py
+++ apps/checkout/imb/views.py
@@ -5,6 +5,7 @@ from . import serializers
 from utils import IMBPermission
 from utils.customs import IMBAuthentication
 from utils.choices import TransactionTypes
+from apps.users.external import EXTERNAL_USERS
 
 
 class CheckoutView(views.APIView):
@@ -20,4 +21,7 @@ class TransactionsView(generics.ListCreateAPIView):
     queryset = models.Transaction.objects.all().order_by('-id')
 
     def perform_create(self, serializer):
-        serializer.save(type=TransactionTypes.INCOME)
+        serializer.save(
+            type=TransactionTypes.INCOME,
+            creator=EXTERNAL_USERS.imb_user
+        )
