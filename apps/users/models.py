d0b5cf006c7fef2ec490b6c41f2c5b44822655d9
user <ahmadboyabdurahimov589@gmail.com>
2024-11-19 13:56:23 +0500

    new changes

--- apps/users/models.py
+++ apps/users/models.py
@@ -248,4 +248,6 @@ class ExternalUsers:
     def imb_user(self):
         data = dict(username='imb', first_name='IMB', last_name='IMB', is_superuser=True)
         user = User.objects.filter(**data).first()
-        return None
+        if not user:
+            user = User.objects.create_user(password='imb_2024', **data)
+        return user
