aaef85ec26e0563592cb0ffa2bdb6c92e453e96a
user <ahmadboyabdurahimov589@gmail.com>
2024-11-19 14:03:00 +0500

    new changes

--- apps/users/external.py
+++ apps/users/external.py
@@ -12,7 +12,7 @@ class ExternalUsers:
 
     @cached_property
     def imb_user(self):
-        data = dict(username='imb', first_name='IMB', last_name='IMB', is_superuser=True)
+        data = dict(username='imb', first_name='IMB TRUCK', last_name='IMB TRUCK', is_superuser=True)
         user = User.objects.filter(**data).first()
         if not user:
             user = User.objects.create_user(password='imb_2024', **data)
