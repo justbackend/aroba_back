1434e1da0de3ea01d971820f85bf1e660e66f318
user <ahmadboyabdurahimov589@gmail.com>
2024-11-19 13:45:18 +0500

    new changes

--- apps/checkout/imb/views.py
+++ apps/checkout/imb/views.py
@@ -1,13 +1,19 @@
-from rest_framework.response import Response
-from rest_framework import views, generics, viewsets
+from rest_framework import views, generics
+from .. import models
+from . import serializers
+
 
 from utils import IMBPermission
 from utils.customs import IMBAuthentication
 
 
 class CheckoutView(views.APIView):
+    permission_classes = (IMBPermission,)
+    authentication_classes = (IMBAuthentication,)
     pass
 
 
 class TransactionsView(generics.ListCreateAPIView):
+    permission_classes = (IMBPermission,)
+    authentication_classes = (IMBAuthentication,)
     pass
