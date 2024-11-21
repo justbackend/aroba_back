00788f506bd2d011747029fc41042a72819bbf0b
user <ahmadboyabdurahimov589@gmail.com>
2024-11-21 20:16:11 +0500

    new changes

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -1,6 +1,42 @@
 from django.contrib import admin
-from unfold import admin as u_admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from django.contrib.auth.admin import GroupAdmin as BaseGroupAdmin
+from django.contrib.auth.models import Group
+from django.utils.translation import gettext_lazy as _
+
 from . import models
+from unfold.admin import ModelAdmin
+from unfold import admin as u_admin
+
+# admin.site.unregister(models.User)
+admin.site.unregister(Group)
+
+
+@admin.register(models.User)
+class UserAdmin(BaseUserAdmin, ModelAdmin):
+    list_display = ("username", "first_name", "last_name", "is_staff")
+    fieldsets = (
+        (None, {"fields": ("username", "password")}),
+        (_("Personal info"), {"fields": ("first_name", "last_name")}),
+        (
+            _("Permissions"),
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                ),
+            },
+        ),
+        (_("Important dates"), {"fields": ("last_login", "date_joined")}),
+    )
+
+
+@admin.register(Group)
+class GroupAdmin(BaseGroupAdmin, ModelAdmin):
+    pass
 
 
 @admin.register(models.Role)
@@ -8,10 +44,10 @@ class RoleAdmin(u_admin.ModelAdmin):
     list_display = ('id', 'name')
 
 
-@admin.register(models.User)
-class UserAdmin(u_admin.ModelAdmin):
-    list_display = ('id', 'username',)
-    readonly_fields = ('actions',)
+# @admin.register(models.User)
+# class UserAdmin(u_admin.ModelAdmin):
+#     list_display = ('id', 'username',)
+#     readonly_fields = ('actions',)
 
 
 @admin.register(models.Action)
