0d01ff9689f2d2466dab6b57ffe7155ba5c27de8
user <ahmadboyabdurahimov589@gmail.com>
2024-11-21 20:05:25 +0500

    new changes

--- apps/imb/admin.py
+++ apps/imb/admin.py
@@ -1,6 +1,25 @@
 from django.contrib import admin
 from unfold import admin as u_admin
+from unfold.contrib.inlines.admin import NonrelatedTabularInline
 from . import models
+
+
+class IMBTransactionsInline(NonrelatedTabularInline):
+    model = models.IMBTransaction
+    extra = 0
+
+    def get_form_queryset(self, obj):
+        """
+        Gets all nonrelated objects needed for inlines. Method must be implemented.
+        """
+        return self.model.objects.all()
+
+    def save_new_instance(self, parent, instance):
+        """
+        Extra save method which can for example update inline instances based on current
+        main model object. Method must be implemented.
+        """
+        pass
 
 
 @admin.register(models.IMBCheckout)
