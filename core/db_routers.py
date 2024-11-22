4cc30a5b44367fdcb8395777eda48543c2a03bf8
user <ahmadboyabdurahimov589@gmail.com>
2024-11-22 14:18:45 +0500

    new changes

--- core/db_routers.py
+++ core/db_routers.py
@@ -1,13 +1,38 @@
-
-
 class DefaultDatabaseRouter:
     """
-    A database router to ensure that migrations are applied only on the 'default' database.
+    A database router to ensure that all database operations for apps other than 'imb'
+    are directed to the 'default' database.
     """
+
+    def db_for_read(self, model, **hints):
+        """
+        Attempts to read data for apps other than 'imb' go to the 'default' database.
+        """
+        if model._meta.app_label != 'imb':
+            return 'default'
+        return None
+
+    def db_for_write(self, model, **hints):
+        """
+        Attempts to write data for apps other than 'imb' go to the 'default' database.
+        """
+        if model._meta.app_label != 'imb':
+            return 'default'
+        return None
+
+    def allow_relation(self, obj1, obj2, **hints):
+        """
+        Allow relations if both models are in the 'default' database or both in the same database
+        other than 'imb'.
+        """
+        if obj1._state.db == 'default' and obj2._state.db == 'default':
+            return True
+        if obj1._meta.app_label != 'imb' and obj2._meta.app_label != 'imb':
+            return True
+        return None
 
     def allow_migrate(self, db, app_label, model_name=None, **hints):
         """
-        Allow migrations only on the 'default' database.
+        Allow migrations only on the 'default' database for apps other than 'imb'.
         """
-
         return db == 'default' and app_label != 'imb'
