0141d38b862f65eed5765287ed35d704a46ae979
user <ahmadboyabdurahimov589@gmail.com>
2024-11-22 16:20:11 +0500

    new changes

--- apps/checkout/views.py
+++ apps/checkout/views.py
@@ -55,7 +55,7 @@ class PayOrder(views.APIView):
                        f"Summa: {order.total_amount}")
         order.create_log(comment=log_comment, action=OrderLogActions.PAID, user=request.user)
 
-        MainCheckout.add(-order.total_amount)
+        MainCheckout - order.total_amount # noqa
 
         return Response(data={'order_id': order_id, 'client_id': order.client_id})
 
@@ -97,6 +97,7 @@ class PayClientOrder(views.APIView):
                        f"{order.car_number}\n"
                        f"{order.driver_phone}\n"
                        f"Summa: {order.income}")
+
         order.create_log(comment=log_comment, action=OrderLogActions.CLIENT_PAID, user=request.user)
 
         return Response(data={'order_id': order_id, 'client_id': order.client_id})
@@ -121,6 +122,7 @@ class RollbackClientPaidOrder(generics.GenericAPIView):
                        f"{order.driver_phone}\n"
                        f"Summa: {order.income}\n"
                        f"Komnetariya: {comment}")
+
         order.create_log(comment=log_comment, action=OrderLogActions.ROLLBACK_CLIENT_PAID, user=request.user)
         return Response(data={'order_id': order_id, 'client_id': order.client_id})
 
