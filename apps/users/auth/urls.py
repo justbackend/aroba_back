cf7ac46e1c5f7ea51bffb80df3a0162725520a5c
user <ahmadboyabdurahimov589@gmail.com>
2024-11-27 17:18:30 +0500

    new changes

--- apps/users/auth/urls.py
+++ apps/users/auth/urls.py
@@ -7,6 +7,7 @@ app_name = 'auth'
 urlpatterns = [
     path('login/', views.LoginView.as_view(), name='login'),
     path('profile/', views.ProfileView.as_view(), name='profile'),
+    path('users/<int:pk>/', views.UpdateUserView.as_view(), name='profile'),
 
     # my permissions
     path('my-perms/', views.MyPermissionsListAPI.as_view(), name='my-perms'),
