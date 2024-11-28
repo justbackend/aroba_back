2b485469ba1f7dd3dfb35cbc9f9cf29983bb8a92
user <ahmadboyabdurahimov589@gmail.com>
2024-11-28 10:07:25 +0500

    new changes

--- apps/users/auth/serializers.py
+++ apps/users/auth/serializers.py
@@ -34,3 +34,4 @@ class UpdateUserSerializer(serializers.ModelSerializer):
         )
 
 
+
--- apps/users/auth/views.py
+++ apps/users/auth/views.py
@@ -2,6 +2,7 @@ from rest_framework import generics, status, views
 from rest_framework.response import Response
 
 from utils.customs.authentication import PayloadAuthentication
+from utils.utility import clear_user_profile_data
 from . import serializers
 from .. import models, utils
 from utils.permissions import PayloadPermission
@@ -22,13 +23,18 @@ class LoginView(generics.GenericAPIView):
 class UpdateUserView(generics.RetrieveUpdateAPIView):
     authentication_classes = (PayloadAuthentication,)
     permission_classes = (PayloadPermission,)
-    http_method_names = ['patch', 'get',]
+    http_method_names = ['patch', 'get', ]
     serializer_class = serializers.UpdateUserSerializer
 
     def get_object(self):
         if not (user_id := self.request.auth.get('user_id')):
             return APIException('User id dont determined')
         return models.User.objects.get(id=user_id)
+
+    def perform_update(self, serializer):
+        serializer.save()
+        obj = serializer.instance
+        clear_user_profile_data(users=(obj))
 
 
 class ProfileView(views.APIView):
