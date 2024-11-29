82125f71a280eb768667205fd2e664fa2b424900
user <ahmadboyabdurahimov589@gmail.com>
2024-11-29 12:06:02 +0500

    new changes

--- core/urls/base.py
+++ core/urls/base.py
@@ -8,7 +8,7 @@ from rest_framework_simplejwt.views import TokenRefreshView
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/", include('apps.api_v1')),
-    path('refresh_token/', TokenRefreshView.as_view()),
+    path('api/v1/refresh_token/', TokenRefreshView.as_view()),
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
     path('docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
