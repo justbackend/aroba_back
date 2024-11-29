c46ed77b21e5fb467c1c0c96c25086624412f941
user <ahmadboyabdurahimov589@gmail.com>
2024-11-29 11:14:36 +0500

    new changes

--- core/urls/base.py
+++ core/urls/base.py
@@ -3,10 +3,12 @@ from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/", include('apps.api_v1')),
+    path('refresh_token/', TokenRefreshView.as_view()),
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
     path('docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
 ]
