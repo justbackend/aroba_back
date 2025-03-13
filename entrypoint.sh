d1dc4269c4824df84bb027c492b7a008b09bc7c5
user <ahmadboyabdurahimov589@gmail.com>
2025-03-13 18:37:45 +0500

    new changes

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,6 +15,6 @@ if not User.objects.filter(username='a').exists():
     User.objects.create_superuser('a', 'a', first_name='Ahmad', last_name='Abdurahimov')
 EOF
 
-exec gunicorn core.wsgi:application --bind 0.0.0.0:8003 --workers 2
+exec gunicorn -c gunicorn.py core.wsgi:application
 
 
---
+++ gunicorn.py
@@ -0,0 +1,8 @@
+workers = 2
+threads = 2
+bind = "0.0.0.0:8003"
+timeout = 30
+loglevel = "error"
+accesslog = "-"
+errorlog = "-"
+keepalive = 2
