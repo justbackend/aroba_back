8516cd4bf97fe39c7a8532006073f6a2648693a3
Abror <izzatullaev2001abror@gmail.com>
2026-06-12 10:15:47 +0500

    initial commit

--- core/settings/production.py
+++ core/settings/production.py
@@ -16,11 +16,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 """
 
 CSRF_COOKIE_SECURE = True
-CSRF_TRUSTED_ORIGINS = [
-    "https://example.com",
-    "https://imb.4fun.uz",
-    'https://api.shoshcargo.uz',
-]
+CSRF_TRUSTED_ORIGINS = ["*"]
 
 
 ###################################################################
